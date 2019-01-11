eeb46285c333420d3839db9af8ccf1514e5b0cbf
David Lynam <dlynam@gmail.com>
2019-01-11 14:32:22 -0800

    update products parser

--- parsers/products.rb
+++ parsers/products.rb
@@ -7,18 +7,20 @@ product = {}
 product['title'] = nokogiri.at_css('.ProductTitle').text.strip
 
 #extract current price
-product['current_price'] = nokogiri.at_css('span.price-characteristic').attr('content')
+product['current_price'] = nokogiri.at_css('span.price-characteristic').attr('content').to_f
 
 #extract original price
 original_price_div = nokogiri.at_css('.price-old')
-product['original_price'] = original_price_div ? original_price_div.text.strip.gsub('$','') : nil
+original_price = original_price_div ? original_price_div.text.strip.gsub('$','').to_f : nil
+product['original_price'] = original_price == 0.0 ? nil : original_price
 
 #extract rating
-product['rating'] = nokogiri.at_css('.hiddenStarLabel .seo-avg-rating').text.strip
+rating = nokogiri.at_css('.hiddenStarLabel .seo-avg-rating').text.strip.to_f
+product['rating'] = rating == 0 ? nil : rating
 
 #extract number of reviews
 review_text = nokogiri.at_css('.stars-reviews-count-node').text.strip
-product['reviews_count'] = review_text =~ /reviews/ ? review_text.split(' ').last : 0
+product['reviews_count'] = review_text =~ /reviews/ ? review_text.split(' ').first.to_i : 0
 
 #extract publisher
 product['publisher'] = nokogiri.at_css('a.prod-brandName').text.strip
